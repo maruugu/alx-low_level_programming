b7953dcce4b697ec8603a554585f060c13e8938a
maruugu <marwa.elgarouadi10@gmail.com>
2023-07-13 19:56:17 -0700

    me

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0 (sucess)
+*/
+int mai(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of the int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)",sizeof(long long int));
+	printf("Size of a float: %lu byte(s)",sizeof(float));
+	return (0);
+}
