845fc2e0b02dd8705bd1de26f1b6eded44142579
maruugu <marwa.elgarouadi10@.com>
2023-07-14 18:18:37 -0700

    me

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,12 @@
  * discription: 'the alphabet in lowercase'
  * Return: always 0
  */
-int main(void);
+int main(void)
+{
 int ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
-{
 putchar(ch);
-
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+putchar(ch);
 return (0);
 }
