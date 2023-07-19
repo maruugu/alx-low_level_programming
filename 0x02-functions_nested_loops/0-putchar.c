b3071f6d8a278401eb2171df17f5b45851e88a0c
maruugu <marwa.elgarouadi10@.com>
2023-07-19 06:26:49 -0700

    me

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * main - Entry point
-* description:'prints _putchar' 
+* description:'prints _putchar'
 * Return: always 0.
 */
 
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Enrty point
+ * description: 'prints the alphabet,in lowercase'
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("alphabet");
+	return (0);
+}
