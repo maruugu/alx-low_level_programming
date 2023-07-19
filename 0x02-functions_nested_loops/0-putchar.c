191f855551e1ee65242fef5a7e6608a8a7366d11
maruugu <marwa.elgarouadi10@.com>
2023-07-18 17:33:39 -0700

    me

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Entry point
+* description:'prints _putchar' 
+* Return: always 0.
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
