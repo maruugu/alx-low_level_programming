3f4868f8de10038be16ec853125a2d8f6459cbf6
maruugu <marwa.elgarouadi10@.com>
2023-07-20 09:17:18 -0700

    me

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,13 @@
 /**
  * _isupper - uppercase letters
  * @c: char to check
- * description: Write a function that checks for uppercase character.
+ * description _isupper : Write a function that checks for uppercase character.
  * return: 0 or 1
  */
 
 int _isupper(int c)
 {
+int c;
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
